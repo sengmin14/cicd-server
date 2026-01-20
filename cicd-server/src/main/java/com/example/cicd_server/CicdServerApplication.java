ea8898245a034381a6770142c2c3582cbd519c00
Jin SeongMin <sengmin14@naver.com>
2026-01-20 19:34:22 +0900

    init

---
+++ cicd-server/src/main/java/com/example/cicd_server/CicdServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdServerApplication.class, args);
+	}
+
+}
---
+++ cicd-server/src/test/java/com/example/cicd_server/CicdServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
