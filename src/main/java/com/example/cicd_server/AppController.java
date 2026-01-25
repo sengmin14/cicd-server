858f8e3cf9fda46ed1ca15780c3f89a8e21a6a0e
Jin SeongMin <sengmin14@naver.com>
2026-01-25 18:57:56 +0900

    init

--- src/main/java/com/example/cicd_server/AppController.java
+++ src/main/java/com/example/cicd_server/AppController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
 
     @GetMapping("/")
-    public String home() {
-        return "Hello world123테스트";
+    public String hello() {
+        return "Hello Worold";
     }
+
 }
